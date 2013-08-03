829ad70be97e931941964ad3d9574fc2582f0c24
Johannes Schickling <schickling.j@gmail.com>
2013-08-03 13:26:25 +0200

    Fixed service provider

--- src/Schickling/Validators/ValidatorsServiceProvider.php
+++ src/Schickling/Validators/ValidatorsServiceProvider.php
@@ -6,16 +6,26 @@ use Validator;
 class ValidatorsServiceProvider extends ServiceProvider {
 
 	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		Validator::extend('boolean', 'Schickling\Validators\Rules\BooleanRule@validate');
+		Validator::extend('optional', 'Schickling\Validators\Rules\OptionalRule@validate');
+		Validator::extend('iban', 'Schickling\Validators\Rules\IbanRule@validate');
+		Validator::extend('bic', 'Schickling\Validators\Rules\BicRule@validate');
+	}
+
+	/**
 	 * Register the service provider.
 	 *
 	 * @return void
 	 */
 	public function register()
 	{
-		Validator::extend('boolean', 'Schickling\Validators\Rules\BooleanRule@validate');
-		Validator::extend('optional', 'Schickling\Validators\Rules\OptionalRule@validate');
-		Validator::extend('iban', 'Schickling\Validators\Rules\IbanRule@validate');
-		Validator::extend('bic', 'Schickling\Validators\Rules\BicRule@validate');
+		// Nothing to be done here :)
 	}
 
 }
